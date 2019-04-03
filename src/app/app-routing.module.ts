6298af66738cb1454169616722b6dff78d7db54f
Reena Koudi <reena.koudi@holition.com>
2019-04-03 21:15:23 +0100

    Router testing for the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { TakeAPicComponent } from 'src/app/components/take-a-pic/take-a-pic.component';
 import { TryOnComponent } from 'src/app/components/try-on/try-on.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   {path: 'tryon' , component: TryOnComponent },
   {path: 'takeapic' , component: TakeAPicComponent },
 ];
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,67 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Location } from '@angular/common';
+import { By } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
+import { RouterOutlet, Router, RouterLinkWithHref, Routes } from '@angular/router';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+import { routingComponents, routes, AppRoutingModule } from './app-routing.module';
 describe('AppComponent', () => {
+  let location: Location;
+  let router: Router;
+  let fixture;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes(routes),
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        routingComponents
       ],
     }).compileComponents();
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+
+    fixture = TestBed.createComponent(AppComponent);
+    router.initialNavigation();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    // const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'Angular-Router-Testing'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    // const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Angular-Router-Testing');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular-Router-Testing!');
+  it('should have a router outlet', () => {
+    // const fixture = TestBed.createComponent(AppComponent);
+    const de = fixture.debugElement.query(By.directive(RouterOutlet));
+    expect(de).not.toBeNull();
   });
+
+  it('navigate to "" redirects you to /', fakeAsync(() => {
+    router.navigate(['']);
+    tick();
+    expect(location.path()).toBe('/');
+  }));
+
+  it('navigate to "Try On" redirects you to /tryon', fakeAsync(() => {
+    router.navigate(['tryon']);
+    tick();
+    expect(location.path()).toBe('/tryon');
+  }));
+
+  it('navigate to "Take A Pic" redirects you to /takeapic', fakeAsync(() => {
+    router.navigate(['takeapic']);
+    tick();
+    expect(location.path()).toBe('/takeapic');
+  }));
+
 });
